9582c5903c559ac2057cafae04df3cecea06d28d
lucca <101908843+kyuven@users.noreply.github.com>
2022-12-20 10:22:15 -0400

    SimpleMove
    Started a simple movement script just to start

--- platformGame/Assets/Scripts/playerMovement.cs
+++ platformGame/Assets/Scripts/playerMovement.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+    private Rigidbody2D rig;
+
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        float movement = Input.GetAxis("Horizontal");
+
+        rig.velocity = new Vector2(movement * speed, rig.velocity.y);
+        if(movement > 0){
+            transform.eulerAngles =  new Vector3(0,0,0);
+        }
+        else if(movement < 0){
+            transform.eulerAngles = new Vector3(0,180,0);
+        }
     }
 }
